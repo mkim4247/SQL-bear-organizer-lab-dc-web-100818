a6f68eb8075e5a5ae31516ada32b0e0e52f3becb
Michael Kim <m.kim4247@gmail.com>
2018-10-15 15:12:38 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,19 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "female", "brown", "angry", "true")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "female", "white", "happy", "false")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "male", "brown", "hungry", "true")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "female", "tan", "contemplative", "false")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 100, "male", "green", "spiteful", "true")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 47, "female", "red", "sassy", "true")
 
-Rowdy, Tabitha, Sergeant Brown, Melissa, Grinch, Wendy, Null 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "male", "black", "tired", "true")
+ Sergeant Brown, Melissa, Grinch, Wendy, Null 
 
 
     name TEXT,
