f9ef42e8d2573c9decd8d81701ac276e3c729cec
Michael Kim <m.kim4247@gmail.com>
2018-10-15 15:09:46 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,6 +11,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "black", "tired", "true")
+
 Rowdy, Tabitha, Sergeant Brown, Melissa, Grinch, Wendy, Null 
 
 
